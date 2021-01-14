7bbbd807be23ea4813552469363109e545f40568
ConnorGuildford <77437903+ConnorGuildford@users.noreply.github.com>
2021-01-14 10:30:15 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+effects.starField.startScreenEffect()
+controller.moveSprite(mySprite)
+info.setScore(0)
+info.changeScoreBy(1)
+game.over(true, effects.dissolve)
